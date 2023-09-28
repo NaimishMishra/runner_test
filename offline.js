d5139dfce2fca219f5fe4756287fdb69d81d9387
Naimish Mishra <naimish2121@gmail.com>
2023-09-28 20:48:52 +0530

    first commit

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1695914251,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/block.png",
+		"images/player-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite2-sheet1.png",
+		"images/hillsscroll-sheet0.png",
+		"images/scroll_bg_far-sheet0.png",
+		"images/left_-sheet0.png",
+		"images/punch-sheet0.png",
+		"images/bomb-sheet0.png",
+		"images/jmp-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
